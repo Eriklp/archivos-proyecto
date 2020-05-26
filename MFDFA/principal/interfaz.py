99f38d8998f38b940645f6567cda1a1e542af610
root <root@localhost.localdomain>
2020-05-25 20:39:32 -0500

    primeras funcionalidades interfaz

--- MFDFA/principal/interfaz.py
+++ MFDFA/principal/interfaz.py
@@ -1,28 +1,4 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMessageBox
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import *
-
-#imports juegos del caos
-import collections
-from collections import OrderedDict
-from matplotlib import pyplot as plt
-from matplotlib import cm
-import pylab
-import math
-from scipy import signal
-import numpy as np
-#f = open('/home/erik/pylori26695_Eslice.fasta')
-#s1 = f.read()
-#data = "".join(s1.split("\n")[1:])
-
-
-#imports para manejo de octave
-
-from oct2py import octave, Oct2Py
-
-#-------------------------------------------------------------------------------------------------------
-lendata = 0
+from interfaz_ui import *
 
 def openFile(fileName):
     if '.fasta' in fileName:
@@ -48,16 +24,10 @@ def openFile(fileName):
                 caracteres.append("C")
         print(data[0:7])
         print(listaNumero[0:7])
-        plt.figure()
-        oc = Oct2Py()
-        oc.myDetrend(caracteres)
-        # x = np.linspace(0, len(listaNumero), 1000)
-        # resultadoDetrend = signal.detrend(x)
-        # print(signal.detrend(listaNumero))
-        # plt.plot(x, resultadoDetrend, label="detrend")
-        # plt.show()
+
     else:
         showdialog("Error", "Debe ser un Archivo Fasta", "Debe ser un archivo Fasta")
+
 
 def showdialog(title, text, inftext):
     msg = QMessageBox()
@@ -73,14 +43,23 @@ def showdialog(title, text, inftext):
     retval = msg.exec_()
     print("value of pressed message box button:", retval)
 
+def abrirArchivo():
+    ventana = MyFileDialog()
+    ventana.initUI()
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+   def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(abrirArchivo)
 
 
 
-class App(QWidget):
-
+class MyFileDialog(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 file dialogs'
+        self.title = 'Seleccionar los archivos .FASTA'
         self.left = 10
         self.top = 10
         self.width = 640
@@ -91,35 +70,36 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.openFileNameDialog()
-        #self.openFileNamesDialog()
+        #self.openFileNameDialog()
+        self.openFileNamesDialog()
         #self.saveFileDialog()
 
         self.show()
 
     def openFileNameDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
         if fileName:
             print(fileName)
             openFile(fileName)
 
     def openFileNamesDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        files, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
         if files:
             print(files)
 
     def saveFileDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = QtWidgets.QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
         if fileName:
             print(fileName)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
